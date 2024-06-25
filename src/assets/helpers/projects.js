c0d0a6d271e27fb7c6fc73a437b316858abf6476
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2024-06-25 20:43:24 -0300

    se realiza map para los proyectos

---
+++ src/assets/helpers/projects.js
@@ -0,0 +1,103 @@
+export const projectData = [
+    // agregar datos en language.js
+    // {
+    //     id: 5,
+    //     texts: {
+    //         es: {
+    //             title: '',
+    //             description:'',
+    //             details: '',
+    //         },  en: {
+    //             title: '',
+    //             description:'',
+    //             details: '',
+    //     }},
+    //     images: [
+    //         "/src/assets/img/projects/nombreProyecto-desktop.webp",
+    //         "/src/assets/img/projects/nombreProyecto-tablet.webp",
+    //         "/src/assets/img/projects/nombreProyecto-mobile.webp",
+    //     ],
+    //     demoUrl: "https://s14-11-m-java-e5su.vercel.app/",
+    //     githubUrl: "https://github.com/No-Country/s14-11-m-java",
+    // },
+    {
+        id: 4,
+        texts: {
+            es: {
+                title: 'EUREKA - Plataforma de cursos y aprendizaje en línea',
+                description:'SIMULACIÓN LABORAL GRUPAL NO-COUNTRY',
+                details: 'Me encargué del desarrollo completo del HOME, BARRA NAVEGACIÓN, FOOTER y EQUIPO. Hice las funcionalidaes del DASHBOARD ALUMNO. Usamos User Stories, Scrum y Trello. Desarrollo en React JS desde un Figma, metodología GitFlow, Estilos TailWind, Responsive y Single Page Application.',
+            },  en: {
+                title: 'EUREKA - Online Courses and Learning Platform',
+                description:'NO COUNTRY GROUP WORK SIMULATION',
+                details: 'I was responsible for the complete development of the HOME, NAVIGATION BAR, FOOTER, and TEAM. I implemented the functionalities of the STUDENT DASHBOARD. We used User Stories, Scrum, and Trello. Development in React JS from a Figma design, GitFlow methodology, TailWind Styles, Responsive design, and Single Page Application.',
+        }},
+        images: [
+            "/src/assets/img/projects/eureka-desktop.webp",
+            "/src/assets/img/projects/eureka-tablet.webp",
+            "/src/assets/img/projects/eureka-mobile.webp",
+        ],
+        demoUrl: "https://s14-11-m-java-e5su.vercel.app/",
+        githubUrl: "https://github.com/No-Country/s14-11-m-java",
+    },
+    {
+        id: 3,
+        texts: {
+            es: {
+                title: 'CAPYBARA WARS - Juego similar a batalla naval. EN PRODUCCIÓN',
+                description:'PROYECTO FINAL DE "CODO A CODO"',
+                details: 'Me encargué del desarrollo completo del Front End y de las User Stories. Hice la lógica del Tablero y del Juego, se guardan los resultados en la Base de Datos. Usamos User Stories, Scrum. Desarrollo en Vainilla JS desde un Figma, metodología GitFlow, Estilos CSS, Responsive.',
+        },  en: {
+                title: 'CAPYBARA WARS - Game similar to Battleship. IN PRODUCTION',
+                description: '"CODO A CODO" Final Project',
+                details: 'I was responsible for the complete development of the Front End. I implemented the logic of the BOARD and GAME in the Front to storage the results on the Database. We used User Stories, Scrum. Development in React JS from a Figma design, GitFlow methodology, CSS Styles, Responsive design.',
+        }},
+        images: [
+            "/src/assets/img/projects/cw-desktop.webp",
+            "/src/assets/img/projects/cw-tablet.webp",
+            "/src/assets/img/projects/cw-mobile.webp"
+        ],
+        demoUrl: "https://capybarawars.netlify.app/",
+        githubUrl: "https://github.com/EmiLongo/capybara-wars",
+    },
+    {
+        id: 2,
+        texts: {
+            es: {
+                title: 'Clone de Tienda de Ropa Zara Colombia 2023',
+                description: 'SIMULACIÓN LABORAL GRUPAL NO-COUNTRY',
+                details: 'Me encargué del diseño del Front End en React JS con Vite. Creado desde un Figma, SEO en el Rendimiento, Estilos con CSS y TailWind, Responsive y Single Page Application.'
+        },  en: {
+                title: "Zara's Colombia Clothing Store Clone 2023",
+                description: 'NO COUNTRY GROUP WORK SIMULATION',
+                details: 'I was in charge of the Front End design in React JS with Vite. Created from a Figma, SEO in Performance, Styles with CSS and TailWind, Responsive and Single Page Application.'
+        }},
+        images: [
+            "/src/assets/img/projects/kara-desktop.webp",
+            "/src/assets/img/projects/kara-tablet.webp",
+            "/src/assets/img/projects/kara-mobile.webp"
+        ],
+        demoUrl: "https://zara-clone-reactjs.netlify.app/",
+        githubUrl: "https://github.com/No-Country/c16-37-m-java-react/",
+    },
+    {
+        id: 1,
+        texts: {
+            es: {
+                title: 'E-commerce del Emprendimiento',
+                description: 'MUNDO ADAPTÓGENOS',
+                details: 'Hice el diseño de branding. Creación de Contenidos e Imágenes, Estructura del Proyecto, SEO en el Rendimiento, Estilos con CSS, Responsive y Mobile First, Desarrollo JavaScript Vainilla.',
+        },  en: {
+                title: 'Entrepreneurship E-commerce',
+                description: 'MUNDO ADAPTÓGENOS',
+                details: 'I did the branding design. Content and Image Creation, Project Structure, SEO Performance, Styles with CSS, Responsive and Mobile First, Vanilla JavaScript Development',
+        }},
+        images: [
+            "/src/assets/img/projects/mundo-desktop.webp",
+            "/src/assets/img/projects/mundo-tablet.webp",
+            "/src/assets/img/projects/mundo-mobile.webp"
+        ],
+        demoUrl: "https://mundoadaptogenos.netlify.app/",
+        githubUrl: "https://github.com/EmiLongo/mundoadaptogenos/",
+    },
+  ];
