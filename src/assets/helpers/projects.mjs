8ca497dfaad97660663553dc5487a0d3a1b42461
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-02-14 20:43:34 -0300

    md helpers-projects - cambios en la estructura de projectData

---
+++ src/assets/helpers/projects.mjs
@@ -0,0 +1,92 @@
+// src/assets/helpers/projects.mjs
+import image1 from "/src/assets/img/projects/mundo-desktop.webp";
+import image2 from "/src/assets/img/projects/mundo-tablet.webp";
+import image3 from "/src/assets/img/projects/mundo-mobile.webp";
+import image4 from "/src/assets/img/projects/kara-desktop.webp";
+import image5 from "/src/assets/img/projects/kara-tablet.webp";
+import image6 from "/src/assets/img/projects/kara-mobile.webp";
+import image7 from "/src/assets/img/projects/cw-desktop.webp";
+import image8 from "/src/assets/img/projects/cw-tablet.webp";
+import image9 from "/src/assets/img/projects/cw-mobile.webp";
+import image10 from "/src/assets/img/projects/eureka-desktop.webp";
+import image11 from "/src/assets/img/projects/eureka-tablet.webp";
+import image12 from "/src/assets/img/projects/eureka-mobile.webp";
+import image13 from "/src/assets/img/projects/xyz-block-desktop.webp";
+import image14 from "/src/assets/img/projects/xyz-block-tablet.webp";
+import image15 from "/src/assets/img/projects/xyz-block-mobile.webp";
+
+// import image16 from 
+// import image17 from 
+// import image18 from 
+// import image19 from 
+// import image20 from 
+// import image21 from 
+// import image22 from 
+// import image23 from 
+// import image24 from 
+// import image25 from 
+    
+
+export const projectData = [
+    // {
+    //     id: 5,
+    //     images: [
+    //         image13,
+    //         image14,
+    //         image15,
+    //     ],
+    //     demoUrl: "https://s14-11-m-java-e5su.vercel.app/",
+    //     githubUrl: "https://github.com/No-Country/s14-11-m-java",
+    // },
+    // ESTÁN COLOCADOS EN EL ORDEN QUE SE QUIEREN MOSTRAR
+    {
+        id: 5,
+        images: [
+            image13,
+            image14,
+            image15,
+        ],
+        demoUrl: "https://clone-blockxyz.netlify.app/",
+        githubUrl: "https://github.com/EmiLongo/clone_of_blockxyz",
+    },
+    {
+        id: 4,
+        images: [
+            image1,
+            image2,
+            image3,
+        ],
+        demoUrl: "https://mundoadaptogenos.netlify.app/",
+        githubUrl: "https://github.com/EmiLongo/mundoadaptogenos/",
+    },
+    {
+        id: 3,
+        images: [
+            image10,
+            image11,
+            image12,
+        ],
+        demoUrl: "https://s14-11-m-java-e5su.vercel.app/",
+        githubUrl: "https://github.com/No-Country/s14-11-m-java",
+    },
+    {
+        id: 2,
+        images: [
+            image4,
+            image5,
+            image6,
+        ],
+        demoUrl: "https://zara-clone-reactjs.netlify.app/",
+        githubUrl: "https://github.com/No-Country/c16-37-m-java-react/",
+    },
+    {
+        id: 1,
+        images: [
+            image7,
+            image8,
+            image9,
+        ],
+        demoUrl: "https://capybarawars.netlify.app/",
+        githubUrl: "https://github.com/EmiLongo/capybara-wars",
+    },
+  ];
