c6b75dc658687c63a7deccda6ccc2571f75cfadc
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-02-14 20:42:00 -0300

    md Portfolio - se agrega TimeLineComponent a la estructura

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,6 +6,7 @@ import ArrowComponent from "./ArrowComponent"
 import GuidesComponent from "./GuidesComponent"
 import FooterComponent from "./FooterComponent"
 import AsideComponent from "./AsideComponent"
+import TimeLineComponent from "./TimeLineComponent"
 // import LoadingComponent from "./LoadingComponent"
 import { LanguageProvider } from "../assets/context/LanguageContext"
 
@@ -19,6 +20,7 @@ const Portfolio = () => {
           <main>
             <HeroComponent />
             <AboutMeComponent />
+            <TimeLineComponent />
             <ProjectsComponent />
             <GuidesComponent />
           </main>
