e41605c7fce503dce9771a14a5d2a55e07310f7b
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-02-14 21:21:54 -0300

    rename languages.js - se mueve a la carpeta helpers, se cambia la estructura

--- src/assets/helpers/languages.js
+++ src/assets/helpers/languages.js
@@ -1,79 +1,130 @@
-//languages
-import {projectData} from './projects.js'
+// src/assets/helpers/languages.js
 export const translation = {
     ES: {
-        heroTextH4: 'Hola!',
-        heroTextH2I: 'Soy',
-        heroTextButton: 'Descargar CV',
-        asideAboutMe: 'ACERCA DE MI',
-        asideProjects: 'PROYECTOS',
-        asideInspiration: 'INSPIRACIÓN',
-        aboutmeH3: 'ACERCA DE MI',
-        aboutmeH4: 'Todo lo que imaginas ',
-        aboutmeH4I: 'lo puedes programar',
-        aboutmeP1: 'Aporto lo mejor de mi desde Rosario, una hermosa ciudad de  Argentina.',
-        aboutmeP2: 'He trabajado por 11 años en empresas multinacionales y he sido emprendedor por 8 años más. Entiendo muy bien como manejarme en grupos interdisciplinarios.',
-        aboutmeP3: 'Busco actualizarme constantemente con nuevas ideas para mejorar proyectos, y además, aprecio mucho el feedback.',
-        aboutmeP4: 'Hago foco en los requerimientos del cliente. Proporciono soluciones efectivas y adaptadas a sus necesidades.',
-        projectsH3: 'PROYECTOS',
-        projectscard0:  {h4: projectData[0].texts.es.title,
-                        p: projectData[0].texts.es.description,
-                        details: projectData[0].texts.es.details},
-        projectscard1:  {h4: projectData[1].texts.es.title,
-                        p: projectData[1].texts.es.description,
-                        details: projectData[1].texts.es.details},
-        projectscard2:  {h4: projectData[2].texts.es.title,
-                        p: projectData[2].texts.es.description,
-                        details: projectData[2].texts.es.details},
-        projectscard3:  {h4: projectData[3].texts.es.title,
-                        p: projectData[3].texts.es.description,
-                        details: projectData[3].texts.es.details},
-        // projectscard4:  {h4: projectData[4].texts.es.title,
-        //                 p: projectData[4].texts.es.description,
-        //                 details: projectData[4].texts.es.details},
-        guidesH3 : 'INSPIRACIÓN Y APRENDIZAJE',
-        footerH3 : '¡Conectemos!',
-        footerP1 : '¿Tienes algún proyecto en mente?',
-        footerP2 : 'Portfolio de Emiliano Longo, 2024',
-        footerButton: 'Enviar email',
-        footerSpan : 'Email copiado!'
+        hero: {
+            h4: 'Hola!',
+            h2i: 'Soy',
+            button: 'Descargar CV'
+        },
+        aside: {
+            aboutMe: 'ACERCA DE MI',
+            experience: 'EXPERIENCIA',
+            projects: 'PROYECTOS',
+            inspiration: 'INSPIRACIÓN'
+        },
+        aboutme: {
+            h3: 'ACERCA DE MI',
+            h4: 'Todo lo que imaginas ',
+            h4i: 'lo puedes programar',
+            p1: 'Aporto lo mejor de mi desde Rosario, una hermosa ciudad de  Argentina.',
+            p2: 'He trabajado por 11 años en empresas multinacionales y he sido emprendedor por 8 años más. Entiendo muy bien como manejarme en grupos interdisciplinarios.',
+            p3: 'Busco actualizarme constantemente con nuevas ideas para mejorar proyectos, y además, aprecio mucho el feedback.',
+            p4: 'Hago foco en los requerimientos del cliente. Proporciono soluciones efectivas y adaptadas a sus necesidades.'
+        },
+        timeline:{
+            h3: 'EXPERIENCIA',
+        },
+        projects: {
+            h3: 'PROYECTOS',
+            // LA NUMERACION DE LA TARJETA TIENE QUE SER EL MISMO QUE EL ID EN PROJECTDATA
+            projectscard5: {
+                h4: 'CLONE DE BLOCK.XYZ',
+                p: 'DESARROLLO WEB CON THREE.JS',
+                details: 'Proyecto técnico desarrollado desde cero como parte de una prueba técnica para un puesto de Front End. Implementación de librerías de Three.js para React, con diseño responsivo y estilos en CSS.'
+            },
+            projectscard4: {
+                h4: 'E-commerce del Emprendimiento',
+                p: 'MUNDO ADAPTÓGENOS',
+                details: 'Hice el diseño de branding. Creación de Contenidos e Imágenes, Estructura del Proyecto, SEO en el Rendimiento, Estilos con CSS, Responsive y Mobile First, Desarrollo JavaScript Vainilla.'
+            },
+            projectscard3: {
+                h4: 'EUREKA - Plataforma de cursos y aprendizaje en línea',
+                p: 'SIMULACIÓN LABORAL GRUPAL NO-COUNTRY',
+                details: 'Me encargué del desarrollo completo del HOME, BARRA NAVEGACIÓN, FOOTER y EQUIPO. Hice las funcionalidaes del DASHBOARD ALUMNO. Usamos User Stories, Scrum y Trello. Desarrollo en React JS desde un Figma, metodología GitFlow, Estilos TailWind, Responsive y Single Page Application.'
+            },
+            projectscard2: {
+                h4: 'Clone de Tienda de Ropa Zara Colombia 2023',
+                p: 'SIMULACIÓN LABORAL GRUPAL NO-COUNTRY',
+                details: 'Me encargué del diseño del Front End en React JS con Vite. Creado desde un Figma, SEO en el Rendimiento, Estilos con CSS y TailWind, Responsive y Single Page Application.'
+            },
+            projectscard1: {
+                h4: 'CAPYBARA WARS - Juego similar a batalla naval. EN PRODUCCIÓN',
+                p: 'PROYECTO FINAL DE "CODO A CODO"',
+                details: 'Me encargué del desarrollo completo del Front End y de las User Stories. Hice la lógica del Tablero y del Juego, se guardan los resultados en la Base de Datos. Usamos User Stories, Scrum. Desarrollo en Vainilla JS desde un Figma, metodología GitFlow, Estilos CSS, Responsive.'
+            }
+        },
+        guides: {
+            h3: 'INSPIRACIÓN Y APRENDIZAJE'
+        },
+        footer: {
+            h3: '¡Conectemos!',
+            p1: '¿Tienes algún proyecto en mente?',
+            p2: 'Portfolio de Emiliano Longo, 2024',
+            button: 'Enviar email',
+            span: 'Email copiado!'
+        }
     },
     EN: {
-        heroTextH4: 'Hi!',
-        heroTextH2I: `I'm`,
-        heroTextButton: 'Download CV',
-        asideAboutMe: 'ABOUT ME',
-        asideProjects: 'PROJECTS',
-        asideInspiration: 'INSPIRATION',
-        aboutmeH3: 'ABOUT ME',
-        aboutmeH4: 'Everything you imagine ',
-        aboutmeH4I: 'you can code it',
-        aboutmeP1: 'I bring out the best of me from Rosario, a beautiful city in Argentina.',
-        aboutmeP2: 'I worked for 11 years in multinational companies, I have been an entrepreneur for 8 more years. I know very well how to handle myself in interdisciplinary groups.',
-        aboutmeP3: 'I constantly seek to update myself with new ideas to improve projects. I highly appreciate feedback.',
-        aboutmeP4: 'I focus on client requirements. I provide effective solutions tailored to their needs.',
-        projectsH3: 'PROJECTS',
-        projectscard0:  {h4: projectData[0].texts.en.title,
-                        p: projectData[0].texts.en.description,
-                        details: projectData[0].texts.en.details},
-        projectscard1:  {h4: projectData[1].texts.en.title,
-                        p: projectData[1].texts.en.description,
-                        details: projectData[1].texts.en.details},
-        projectscard2:  {h4: projectData[2].texts.en.title,
-                        p: projectData[2].texts.en.description,
-                        details: projectData[2].texts.en.details},
-        projectscard3:  {h4: projectData[3].texts.en.title,
-                        p: projectData[3].texts.en.description,
-                        details: projectData[3].texts.en.details},
-        // projectscard4:  {h4: projectData[4].texts.en.title,
-        //                 p: projectData[4].texts.en.description,
-        //                 details: projectData[4].texts.en.details},
-        guidesH3 : 'INSPIRATION AND LEARNING',
-        footerH3 : `Let's Connect!`,
-        footerP1 : 'Are you minded a project?',
-        footerButton: 'Send email',
-        footerP2 : `Emiliano Longo's Portfolio, 2024`,
-        footerSpan : 'Email copied!'
-
+        hero: {
+            h4: 'Hi!',
+            h2i: "I'm",
+            button: 'Download CV'
+        },
+        aside: {
+            aboutMe: 'ABOUT ME',
+            experience: 'EXPERIENCE',
+            projects: 'PROJECTS',
+            inspiration: 'INSPIRATION'
+        },
+        aboutme: {
+            h3: 'ABOUT ME',
+            h4: 'Everything you imagine ',
+            h4i: 'you can code it',
+            p1: 'I bring out the best of me from Rosario, a beautiful city in Argentina.',
+            p2: 'I worked for 11 years in multinational companies, I have been an entrepreneur for 8 more years. I know very well how to handle myself in interdisciplinary groups.',
+            p3: 'I constantly seek to update myself with new ideas to improve projects. I highly appreciate feedback.',
+            p4: 'I focus on client requirements. I provide effective solutions tailored to their needs.'
+        },
+        timeline:{
+            h3: 'EXPERIENCE',
+        },
+        projects: {
+            h3: 'PROJECTS',
+            projectscard5: {
+                h4: "BLOCK.XYZ's CLONE",
+                p: 'WEB DEVELOPMENT WITH THREE.JS',
+                details: 'Technical project developed from scratch as part of a technical assessment for a Front-End position. Implemented Three.js libraries for React, with responsive design and CSS styling.'
+            },
+            projectscard4: {
+                h4: 'Entrepreneurship E-commerce',
+                p: 'MUNDO ADAPTÓGENOS',
+                details: 'I did the branding design. Content and Image Creation, Project Structure, SEO Performance, Styles with CSS, Responsive and Mobile First, Vanilla JavaScript Development'
+            },
+            projectscard3: {
+                h4: 'EUREKA - Online Courses and Learning Platform',
+                p: 'NO COUNTRY GROUP WORK SIMULATION',
+                details: 'I was responsible for the complete development of the HOME, NAVIGATION BAR, FOOTER, and TEAM. I implemented the functionalities of the STUDENT DASHBOARD. We used User Stories, Scrum, and Trello. Development in React JS from a Figma design, GitFlow methodology, TailWind Styles, Responsive design, and Single Page Application.'
+            },
+            projectscard2: {
+                h4: "Zara's Colombia Clothing Store Clone 2023",
+                p: 'NO COUNTRY GROUP WORK SIMULATION',
+                details: 'I was in charge of the Front End design in React JS with Vite. Created from a Figma, SEO in Performance, Styles with CSS and TailWind, Responsive and Single Page Application.'
+            },
+            projectscard1: {
+                h4: 'CAPYBARA WARS - Game similar to Battleship. IN PRODUCTION',
+                p: '"CODO A CODO" Final Project',
+                details: 'I was responsible for the complete development of the Front End. I implemented the logic of the BOARD and GAME in the Front to storage the results on the Database. We used User Stories, Scrum. Development in React JS from a Figma design, GitFlow methodology, CSS Styles, Responsive design.'
+            }
+        },
+        guides: {
+            h3: 'INSPIRATION AND LEARNING'
+        },
+        footer: {
+            h3: "Let's Connect!",
+            p1: 'Are you minded a project?',
+            p2: "Emiliano Longo's Portfolio, 2024",
+            button: 'Send email',
+            span: 'Email copied!'
+        }
     }
 }
