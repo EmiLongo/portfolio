863e64299ea49cfcdb21e5d2b3a4466a44ea0332
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-02-14 21:25:32 -0300

    md AboutMeComponent - se actualiza con la estructura de languages

--- src/components/AboutMeComponent.jsx
+++ src/components/AboutMeComponent.jsx
@@ -5,14 +5,14 @@ const AboutMeComponent = () => {
   const { texts } = useContext(LanguageContext)
   return (
     <div className="aboutme">
-        <h3>{texts.aboutmeH3}</h3><i id="aboutMeSection"></i>
+        <h3>{texts.aboutme.h3}</h3><i id="aboutMeSection"></i>
         <div className="aboutme-text-container">
             <div className="aboutme-text">
-                <h4>{texts.aboutmeH4}<i>{texts.aboutmeH4I}</i></h4>
-                <p>{texts.aboutmeP1}</p>
-                <p>{texts.aboutmeP2}</p>
-                <p>{texts.aboutmeP3}</p>
-                <p>{texts.aboutmeP4}</p>
+                <h4>{texts.aboutme.h4}<i>{texts.aboutme.h4I}</i></h4>
+                <p>{texts.aboutme.p1}</p>
+                <p>{texts.aboutme.p2}</p>
+                <p>{texts.aboutme.p3}</p>
+                <p>{texts.aboutme.p4}</p>
             </div>
         </div>
     </div>
