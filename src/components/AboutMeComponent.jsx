a171757c56eb31423c29bd50f4abbf4a1e6a1a89
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-02-24 03:14:18 -0300

    fix AboutMeComponent - ahora si muestra la segunda parte del about me

--- src/components/AboutMeComponent.jsx
+++ src/components/AboutMeComponent.jsx
@@ -8,7 +8,7 @@ const AboutMeComponent = () => {
         <h3>{texts.aboutme.h3}</h3><i id="aboutMeSection"></i>
         <div className="aboutme-text-container">
             <div className="aboutme-text">
-                <h4>{texts.aboutme.h4}<i>{texts.aboutme.h4I}</i></h4>
+                <h4>{texts.aboutme.h4}<i>{texts.aboutme.h4i}</i></h4>
                 <p>{texts.aboutme.p1}</p>
                 <p>{texts.aboutme.p2}</p>
                 <p>{texts.aboutme.p3}</p>
